38e35a4c2c2566169cd68891ae8789b1c4707c69
tyler1218hatch <tyler1218hatch@gmail.com>
2020-06-28 21:17:40 -0500

    Incorporated Step 5

--- Network_Extraction.py
+++ Network_Extraction.py
@@ -24,6 +24,7 @@ def main():
 
     reach_preparation(root_folder, fixed_points)
     reach_editing(root_folder)
+    data_cleaning(root_folder)
 
 
 def reach_preparation(root_folder, fixed_points):
@@ -329,6 +330,85 @@ def delete_segment(segment, point_id, tor_bor, reach_id):
         return True
 
     return False
+
+
+def data_cleaning(root_folder):
+
+    # Initialize variables and file locations
+    arcpy.env.overwriteOutput = True
+
+    watershed_folders = PNET_Functions.get_watershed_folders(root_folder)
+    projectwide_output = os.path.join(root_folder, "00_ProjectWide", "Intermediates", "Extraction", "Inputs")
+    temps_to_delete = []
+    keep_fields = ["Shape", "FID", "SiteID", "RchID", "POINT_X", "POINT_Y", "SnapDist"]
+    to_merge_reaches = []
+    to_merge_points = []
+    PNET_Functions.delete_old(os.path.join(root_folder, "00_ProjectWide", "Inputs", "Parameters"))
+
+
+    PNET_Functions.delete_old(projectwide_output)
+
+    for watershed in watershed_folders:
+        arcpy.AddMessage("Starting {}...".format(watershed))
+
+        # Get necessary files
+        output_folder = os.path.join(watershed, "Intermediates", "Extraction", "Inputs")
+        in_reaches = os.path.join(watershed, "Intermediates", "Reach_Editing", "Outputs", "Field_Reaches.shp")
+        in_points = os.path.join(watershed, "Intermediates", "Reach_Editing", "Outputs", "Field_Points.shp")
+        points_temp = os.path.join(output_folder, "p_temp.shp")
+        reaches_temp = os.path.join(output_folder, "r_temp.shp")
+        reaches_joined = os.path.join(output_folder, "r_join.shp")
+        points_joined = os.path.join(output_folder, "p_join.shp")
+        temps_to_delete.extend([points_temp, reaches_temp, reaches_joined, points_joined])
+        points_final = os.path.join(output_folder, "Field_Points_Clean.shp")
+        reaches_final = os.path.join(output_folder, "Field_Reaches_Clean.shp")
+
+        # Add field for the length of the field reach
+        arcpy.Copy_management(in_reaches, reaches_temp)
+        field_to_add = "FldRchLen"
+        keep_fields.append(field_to_add)
+        field_names = [f.name for f in arcpy.ListFields(reaches_temp)]
+
+        if field_to_add not in field_names:
+            arcpy.AddField_management(reaches_temp, field_to_add, "DOUBLE")
+
+        arcpy.CalculateField_management(reaches_temp, field_to_add, "!shape.length@meters!", "PYTHON_9.3", "")
+
+        # Reduce points to only BOR points
+        points_layer = "Points"
+        arcpy.MakeFeatureLayer_management(in_points, points_layer)
+        arcpy.SelectLayerByAttribute_management(points_layer, 'NEW_SELECTION', "\"TOR_BOR\" = \'BOR\'")
+        arcpy.CopyFeatures_management(points_layer, points_temp)
+
+        # Add all point data to the reaches
+        arcpy.SpatialJoin_analysis(reaches_temp, points_temp, reaches_joined, "JOIN_ONE_TO_ONE")
+
+        # Add all reach data to the points
+        arcpy.SpatialJoin_analysis(points_temp, reaches_temp, points_joined, "JOIN_ONE_TO_ONE")
+
+        # Only keep fields we need
+        shapes = [points_joined, reaches_joined]
+        for shape in shapes:
+
+            # Removes all fields from the shapefile that are not in the above list of fields to keep
+            field_names = [f.name for f in arcpy.ListFields(shape)]
+            delete_fields = []
+            for field in field_names:
+                if field not in keep_fields:
+                    delete_fields.append(field)
+
+            arcpy.DeleteField_management(shape, delete_fields)
+
+        # Save the points and reaches
+        to_merge_points.append(arcpy.CopyFeatures_management(points_joined, points_final))
+        to_merge_reaches.append(arcpy.CopyFeatures_management(reaches_joined, reaches_final))
+
+    arcpy.AddMessage("Saving Projectwide...")
+    arcpy.Merge_management(to_merge_points, os.path.join(projectwide_output, "Field_Points_Clean"))
+    arcpy.Merge_management(to_merge_reaches, os.path.join(projectwide_output, "Field_Reaches_Clean"))
+
+    PNET_Functions.delete_temps(temps_to_delete)
+    PNET_Functions.finish()
 
 
 if __name__ == "__main__":
