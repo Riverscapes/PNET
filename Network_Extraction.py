19a739078f3154ae7b4a523e9e193ec4ee4eec23
tyler1218hatch <tyler1218hatch@gmail.com>
2020-06-28 21:19:38 -0500

    Incorporated Step 6

--- Network_Extraction.py
+++ Network_Extraction.py
@@ -18,6 +18,8 @@ root_folder = arcpy.GetParameterAsText(0)
 # If True, this indicates that the user edited the unsnapped points, and the corrections are now saved in
 # ProjectWide/Intermediates/Points/Unsnapped_Fixed
 fixed_points = PNET_Functions.parse_bool(arcpy.GetParameterAsText(1))
+# A list of links to shapefiles that contain entire model runs
+data_networks_list_in = PNET_Functions.parse_multistring(arcpy.GetParameterAsText(2))
 
 
 def main():
@@ -408,6 +410,67 @@ def data_cleaning(root_folder):
     arcpy.Merge_management(to_merge_reaches, os.path.join(projectwide_output, "Field_Reaches_Clean"))
 
     PNET_Functions.delete_temps(temps_to_delete)
+    PNET_Functions.finish()
+
+
+def data_network_input(root_folder, data_networks_list_in):
+
+    # Initialize variables and file locations
+    arcpy.env.overwriteOutput = True
+
+    watershed_folders = PNET_Functions.get_watershed_folders(root_folder)
+    projectwide_output = os.path.join(root_folder, "00_ProjectWide", "Inputs", "Data_Networks")
+    projectwide_network = os.path.join(root_folder, "00_ProjectWide",  "Inputs", "Stream_Network", "Stream_Network.shp")
+    PNET_Functions.delete_old(projectwide_output)
+    total_count = len(data_networks_list_in)
+
+    network_names = []
+
+    for _ in data_networks_list_in:
+        network_names.append([])
+
+    for data_network, network_slot in zip(data_networks_list_in, network_names):
+        name = data_network.split("\\")[-1]
+        new_name = name.replace(".shp", "")
+        network_slot.append(new_name)
+        network_slot.append(data_network)
+
+    sorted_list = sorted(network_names, key=lambda s: s[0].lower())
+
+    for watershed in watershed_folders:
+        # Clear old data
+        PNET_Functions.delete_old(os.path.join(watershed, "Inputs", "Data_Networks"))
+
+    for current_count, network_data in enumerate(sorted_list):
+
+        name = network_data[0]
+        network = network_data[1]
+        arcpy.AddMessage("\nSaving {} Files ({}/{})...".format(name, current_count+1, total_count))
+
+        if '.shp' not in name:
+            name += '.shp'
+
+        for watershed in watershed_folders:
+
+            arcpy.AddMessage("\tStarting " + watershed + "...")
+
+            # Get network to clip by
+            old_stream_network = os.path.join(watershed, "Inputs", "Stream_Network", "Stream_Network.shp")
+
+            # Clip the current data network to this watershed
+            new_network_save = os.path.join(watershed, "Inputs", "Data_Networks", name)
+            arcpy.Clip_analysis(network, old_stream_network, new_network_save)
+
+
+            # Don't create an empty shapefile
+            if PNET_Functions.is_empty(new_network_save):
+                arcpy.AddMessage("Did not save {}, as it was empty".format(new_network_save))
+                arcpy.Delete_management(new_network_save)
+
+        arcpy.AddMessage("\tSaving Projectwide...")
+        new_network_save = os.path.join(projectwide_output, name)
+        arcpy.Clip_analysis(network, projectwide_network, new_network_save)
+
     PNET_Functions.finish()
 
 
